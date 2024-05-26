cfca86ca13181010b8180565b78a8d4a8917b3db
Sun Xuan @MBP-SP-SX <songen.sunxuan@gmail.com>
2024-05-27 01:00:08 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import DemoA from './components/DemoA.vue'
+import { register } from 'swiper/element/bundle'
+import 'swiper/css'
+register()
+</script>
+
+<template>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <DemoA msg="Demo A" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/DemoA.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+</script>
+
+<template>
+  <swiper-container
+    :slides-per-view="1"
+    :space-between="spaceBetween"
+    :centered-slides="true"
+    :navigation="true"
+    :pagination="{
+      hideOnClick: false,
+    }"
+    :loop="true"
+    :breakpoints="{
+      768: {
+        slidesPerView: 1,
+      },
+    }"
+    @swiperprogress="onProgress"
+    @swiperslidechange="onSlideChange"
+  >
+    <swiper-slide>
+      <div class="slide-page">Slide 1</div>
+    </swiper-slide>
+    <swiper-slide>
+      <div class="slide-page">Slide 2</div>
+    </swiper-slide><swiper-slide>
+      <div class="slide-page">Slide 3</div>
+    </swiper-slide><swiper-slide>
+      <div class="slide-page">Slide 4</div>
+    </swiper-slide>
+  </swiper-container>
+</template>
+
+<style scoped>
+.slide-page {
+  width: 100%;
+  height: 500px;
+  background: red;
+}
+</style>
